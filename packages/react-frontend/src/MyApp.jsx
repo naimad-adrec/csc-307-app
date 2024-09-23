57a65993d8b2b6ebaf93b631093e133ef1d77932
naimad-adrec <106353663+naimad-adrec@users.noreply.github.com>
2024-09-23 15:33:56 -0700

    Create Form component
    - Created the Form component which will be used to manage data related to a
      single character
    - Removed hard-coded data

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,13 +1,9 @@
 import React, {useState} from "react";
 import Table from "./Table.jsx";
+import Form from "./Form.jsx";
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-        {
-            name: "Charlie",
-            job: "Janitor" // the rest of the data
-        }
-    ]);
+    const [characters, setCharacters] = useState([]);
 
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
@@ -22,6 +18,7 @@ function MyApp() {
                 characterData={characters}
                 removeCharacter={removeOneCharacter}
             />
+            <Form />
         </div>
     );
 }
