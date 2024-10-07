ffb7d689fe5d77d6beb74e239736d6002e65fea7
naimad-adrec <106353663+naimad-adrec@users.noreply.github.com>
2024-10-07 16:07:45 -0700

    Update back-end users through front-end
    - You can now add users through the front-end and it will persist within the backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
 
-app.use(cors());
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 const users = {
